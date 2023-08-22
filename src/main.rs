c0e8050d0f195401efbe1148fc0c2038b43c30ca
archification <archification@gmail.com>
2023-08-22 12:15:43 -0400

    gpt improvement session

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
 mod solarized;
 mod stuff;
 
-use crossterm::style::{SetBackgroundColor, SetForegroundColor, ResetColor};
-use solarized::{BACK, VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA};
+use solarized::{print_colored, VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA};
 use stuff::{read_config, print_log_contents};
 
 fn main() {
-    println!(
-        "{}{}Welcome {}to {}the {}warframe {}log {}parser {}of {}doom.{}",
-        SetBackgroundColor(BACK),
-        SetForegroundColor(VIOLET),
-        SetForegroundColor(BLUE),
-        SetForegroundColor(CYAN),
-        SetForegroundColor(GREEN),
-        SetForegroundColor(YELLOW),
-        SetForegroundColor(ORANGE),
-        SetForegroundColor(RED),
-        SetForegroundColor(MAGENTA),
-        ResetColor
+    print_colored(
+        &["Welcome", "to", "the", "warframe", "log", "parser", "of", "doom."],
+        &[VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA]
     );
     read_config();
     print_log_contents();
--- src/stuff.rs
+++ src/stuff.rs
@@ -3,18 +3,18 @@ use std::io::{BufRead, BufReader, SeekFrom, Seek};
 use std::thread;
 use std::time::Duration;
 use std::sync::RwLock;
+use std::path::PathBuf;
 use toml;
 use serde::Deserialize;
-use crossterm::style::{SetBackgroundColor, SetForegroundColor, ResetColor};
-use crate::solarized::{BACK, VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA};
+use crate::solarized::{print_colored, VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA};
 
 lazy_static::lazy_static! {
-    static ref LOG_FILE_PATH: RwLock<String> = RwLock::new(String::new());
+    static ref LOG_FILE_PATH: RwLock<PathBuf> = RwLock::new(PathBuf::new());
 }
 
 #[derive(Deserialize)]
 struct Config {
-    log_file_path: String,
+    log_file_path: PathBuf,
 }
 
 pub fn read_config() {
@@ -24,49 +24,28 @@ pub fn read_config() {
         .expect("Failed to parse config file.");
     let mut path = LOG_FILE_PATH.write().unwrap();
     *path = config.log_file_path;
-    println!("{}{}f{}i{}l{}e {}r{}e{}a{}d{}: Config OK",
-        SetBackgroundColor(BACK),
-        SetForegroundColor(VIOLET),
-        SetForegroundColor(BLUE),
-        SetForegroundColor(CYAN),
-        SetForegroundColor(GREEN),
-        SetForegroundColor(YELLOW),
-        SetForegroundColor(ORANGE),
-        SetForegroundColor(RED),
-        SetForegroundColor(MAGENTA),
-        ResetColor
+    print_colored(
+        &["f", "i", "l", "e ", "r", "e", "a", "d", ": Config OK"],
+        &[VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA]
     );
 }
 
 pub fn print_log_contents() {
-    let path: String = LOG_FILE_PATH.read().unwrap().clone();
-    println!("{}", path);
+    let path = LOG_FILE_PATH.read().unwrap().clone();
     let mut reader = BufReader::new(File::open(&path).expect("Log file cannot be read."));
     thread::spawn(move || {
         let last_pos = 0;
         loop {
             let mut file = File::open(&path).expect("Unable to open log file.");
             file.seek(SeekFrom::Start(last_pos)).expect("Unable to seek log file.");
-            for line in (&mut reader).lines().skip(last_pos as usize) {
+            for line in (&mut reader).lines() {
                 if let Ok(line) = line {
                     if line.contains("killed") {
-                        let parts = line.split("killed");
-                        let collection = parts.collect::<Vec<&str>>();
-                        if let Some(first) = collection.get(0) {
-                            let first_string = (*first).to_string();
-                            if let Some(second) = collection.get(1) {
-                                let second_string = (*second).to_string();
-                                println!(
-                                    "{}{}{}{}killed{}{}{}",
-                                    SetBackgroundColor(BACK),
-                                    SetForegroundColor(CYAN),
-                                    first_string,
-                                    SetForegroundColor(RED),
-                                    SetForegroundColor(CYAN),
-                                    second_string,
-                                    ResetColor
-                                );
-                            }
+                        if let Some((before, after)) = line.split_once("killed") {
+                            print_colored(
+                                &[before, "killed", after],
+                                &[CYAN, RED, CYAN]
+                            );
                         }
                     }
                 }
