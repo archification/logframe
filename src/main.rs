163b5fee586d8efd5b57b3df5e7bee3a5873a39c
archification <archification@gmail.com>
2023-09-01 22:11:11 -0400

    improvement session 2

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod stuff;
 
 use solarized::{print_colored, VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA};
 use stuff::{read_config, print_log_contents};
+use std::sync::Arc;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     print_colored(
         &["Welcome", "to", "the", "warframe", "log", "parser", "of", "doom."],
         &[VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA]
     );
-    read_config();
-    print_log_contents();
+    let path = Arc::new(read_config()?);
+    print_log_contents(path);
+    Ok(())
 }
--- src/stuff.rs
+++ src/stuff.rs
@@ -2,57 +2,46 @@ use std::fs::{self, File};
 use std::io::{BufRead, BufReader, SeekFrom, Seek};
 use std::thread;
 use std::time::Duration;
-use std::sync::RwLock;
+use std::sync::Arc;
 use std::path::PathBuf;
 use toml;
 use serde::Deserialize;
 use crate::solarized::{print_colored, VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA};
-
-lazy_static::lazy_static! {
-    static ref LOG_FILE_PATH: RwLock<PathBuf> = RwLock::new(PathBuf::new());
-}
 
 #[derive(Deserialize)]
 struct Config {
     log_file_path: PathBuf,
 }
 
-pub fn read_config() {
-    let config_str = fs::read_to_string("config.toml")
-        .expect("Config file cannot be read.");
-    let config: Config = toml::from_str(&config_str)
-        .expect("Failed to parse config file.");
-    let mut path = LOG_FILE_PATH.write().unwrap();
-    *path = config.log_file_path;
+pub fn read_config() -> Result<PathBuf, Box<dyn std::error::Error>> {
+    let config_str = fs::read_to_string("config.toml")?;
+    let config: Config = toml::from_str(&config_str)?;
     print_colored(
         &["f", "i", "l", "e ", "r", "e", "a", "d", ": Config OK"],
         &[VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA]
     );
+    Ok(config.log_file_path)
 }
 
-pub fn print_log_contents() {
-    let path = LOG_FILE_PATH.read().unwrap().clone();
-    let mut reader = BufReader::new(File::open(&path).expect("Log file cannot be read."));
-    thread::spawn(move || {
-        let last_pos = 0;
-        loop {
-            let mut file = File::open(&path).expect("Unable to open log file.");
-            file.seek(SeekFrom::Start(last_pos)).expect("Unable to seek log file.");
-            for line in (&mut reader).lines() {
-                if let Ok(line) = line {
-                    if line.contains("killed") {
-                        if let Some((before, after)) = line.split_once("killed") {
-                            print_colored(
-                                &[before, "killed", after],
-                                &[CYAN, RED, CYAN]
-                            );
-                        }
+pub fn print_log_contents(path: Arc<PathBuf>) {
+    let mut reader = BufReader::new(File::open(&*path).unwrap());
+    let last_pos = 0;
+    loop {
+        let mut file = File::open(&*path).unwrap();
+        file.seek(SeekFrom::Start(last_pos)).unwrap();
+        for line in (&mut reader).lines() {
+            if let Ok(line) = line {
+                if line.contains("killed") {
+                    if let Some((before, after)) = line.split_once("killed") {
+                        print_colored(
+                            &[before, "killed", after],
+                            &[CYAN, RED, CYAN]
+                        );
                     }
                 }
             }
-            let _last_pos = file.metadata().map(|m| m.len() as u64).unwrap_or(last_pos);
-            thread::sleep(Duration::from_secs(1));
         }
-    });
-    loop {}
+        let _last_pos = file.metadata().map(|m| m.len() as u64).unwrap_or(last_pos);
+        thread::sleep(Duration::from_secs(1));
+    }
 }
