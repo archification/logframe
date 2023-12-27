7768384e5b8652ef917a2d28d5a43cc707c08ac6
archification <archification@gmail.com>
2023-12-26 21:58:32 -0500

    something

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod common;
 mod stuff;
 
+use std::env;
 use solarized::{
-    print_colored, PrintMode::NewLine,
-    VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA
+    print_colored, print_fancy, PrintMode::NewLine,
+    VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA, GREY
 };
 use stuff::{read_config, print_log_contents};
 use std::sync::Arc;
@@ -11,6 +12,34 @@ use common::clear;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     clear();
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 2 {
+        print_fancy(&[
+            ("config.toml", BLUE, vec![]),
+            (" file has these three(3) fields.\n", CYAN, vec![]),
+            ("log_file_path", GREY, vec![]),
+            (" indicates the full path to the ", CYAN, vec![]),
+            ("EE.log", BLUE, vec![]),
+            (" file.\n", CYAN, vec![]),
+            ("search_words", GREEN, vec![]),
+            (" indicates which words to search for in each line.\n", CYAN, vec![]),
+            ("usernames", VIOLET, vec![]),
+            (" indicates which usernames to search for in each line.\n", CYAN, vec![]),
+        ], NewLine);
+    } else {
+        print_colored(
+            &["Welcome ", "to ", "the ", "warframe ", "log ", "parser ", "of ", "doom."],
+            &[VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA],
+            NewLine
+        );
+        let config = read_config()?;
+        let search_words = config.search_words.clone();
+        let usernames = config.usernames.clone();
+        let path = Arc::new(config.log_file_path);
+        print_log_contents(path, search_words, usernames);
+    }
+    Ok(())
+    /*
     print_colored(
         &["Welcome ", "to ", "the ", "warframe ", "log ", "parser ", "of ", "doom."],
         &[VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA],
@@ -22,4 +51,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = Arc::new(config.log_file_path);
     print_log_contents(path, search_words, usernames);
     Ok(())
+    */
 }
--- src/stuff.rs
+++ src/stuff.rs
@@ -43,24 +43,32 @@ pub fn print_log_contents(path: Arc<PathBuf>, search_words: Vec<String>, usernam
                 if contains_killed && contains_username {
                     if let Some((before_killed, after_killed)) = line.split_once("killed") {
                         for username in &usernames {
-                            if let Some((before_username, after_username)) = before_killed.split_once(username) {
-                                print_fancy(&[
-                                    (before_username, CYAN, vec![]),
-                                    (username, VIOLET, vec![BOLD]),
-                                    (after_username, CYAN, vec![]),
-                                    ("killed", RED, vec![BOLD, UNDERLINED, ITALIC]),
-                                    (after_killed, CYAN, vec![]),
-                                ], NewLine);
-                                break;
+                            if before_killed.contains(username) {
+                                let busersplit = before_killed.split_once(username);
+                                if let Some((before_username, after_username)) = busersplit {
+                                    print_fancy(&[
+                                        (before_username, CYAN, vec![]),
+                                        (username, VIOLET, vec![BOLD]),
+                                        (after_username, CYAN, vec![]),
+                                        ("killed", RED, vec![BOLD, UNDERLINED, ITALIC]),
+                                        (after_killed, CYAN, vec![]),
+                                    ], NewLine);
+                                    break;
+                                }
+                            } else if after_killed.contains(username) {
+                                let ausersplit = after_killed.split_once(username);
+                                if let Some((before_username, after_username)) = ausersplit {
+                                    print_fancy(&[
+                                        (before_killed, CYAN, vec![]),
+                                        ("killed", RED, vec![]),
+                                        (before_username, CYAN, vec![]),
+                                        (username, VIOLET, vec![BOLD]),
+                                        (after_username, CYAN, vec![]),
+                                    ], NewLine);
+                                    break;
+                                }
                             }
                         }
-                        /*
-                        print_fancy(&[
-                            (before_killed, CYAN, vec![]),
-                            ("killed", RED, vec![BOLD, UNDERLINED, ITALIC]),
-                            (after_killed, CYAN, vec![]),
-                        ], NewLine);
-                        */
                     }
                 } else {
                     for word in &search_words {
@@ -68,7 +76,7 @@ pub fn print_log_contents(path: Arc<PathBuf>, search_words: Vec<String>, usernam
                             if let Some((before, after)) = line.split_once(word) {
                                 print_fancy(&[
                                     (before, CYAN, vec![]),
-                                    (word, VIOLET, vec![BOLD, UNDERLINED, ITALIC]),
+                                    (word, GREEN, vec![BOLD, UNDERLINED, ITALIC]),
                                     (after, CYAN, vec![]),
                                 ], NewLine);
                             }
