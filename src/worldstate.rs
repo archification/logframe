775a00777a0d8748f8886283e32f813fc76ee150
archification <archification@gmail.com>
2025-08-01 17:10:23 -0400

    solarized worldstate output

--- src/worldstate.rs
+++ src/worldstate.rs
@@ -1,5 +1,10 @@
 use serde::Deserialize;
 use std::error::Error;
+use solarized::{
+    print_fancy, PrintMode::NewLine,
+    VIOLET, BLUE, CYAN, GREEN,
+    //YELLOW, ORANGE, RED, MAGENTA, GREY
+};
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -32,20 +37,44 @@ struct CambionCycle {
 
 pub async fn worldstate() -> Result<(), Box<dyn Error>> {
     const API_URL: &str = "https://api.warframestat.us/pc/";
-    println!("Fetching Warframe open world cycle status...");
+    print_fancy(&[
+        ("Fetching Warframe open world cycle status...", CYAN, vec![]),
+    ], NewLine);
     let response = reqwest::get(API_URL).await?;
     if !response.status().is_success() {
         return Err(format!("API request failed with status: {}", response.status()).into());
     }
     let body = response.text().await?;
     let world_state: WorldState = serde_json::from_str(&body)?;
-    println!("\n----------------------------------------");
     let cetus_state = if world_state.cetus_cycle.is_day { "Day" } else { "Night" };
+    let vallis_state = if world_state.vallis_cycle.is_warm { "Warm" } else { "Cold" };
+    print_fancy(&[
+        ("\n-----\n", CYAN, vec![]),
+        ("Plains of Eidolon (Cetus)\n", VIOLET, vec![]),
+        ("  State: ", CYAN, vec![]),
+        (&format!("{}\n", cetus_state), GREEN, vec![]),
+        ("  Time until change: ", CYAN, vec![]),
+        (&world_state.cetus_cycle.time_left, BLUE, vec![]),
+        ("\n-----\n", CYAN, vec![]),
+        ("Orb Vallis\n", VIOLET, vec![]),
+        ("  State: ", CYAN, vec![]),
+        (&format!("{}\n", vallis_state), GREEN, vec![]),
+        ("  Time until change: ", CYAN, vec![]),
+        (&world_state.vallis_cycle.time_left, BLUE, vec![]),
+        ("\n-----\n", CYAN, vec![]),
+        ("Cambion Drift (Deimos)\n", VIOLET, vec![]),
+        ("  State: ", CYAN, vec![]),
+        (&format!("{}\n", world_state.cambion_cycle.active.to_uppercase()), GREEN, vec![]),
+        ("  Time until change: ", CYAN, vec![]),
+        (&world_state.cambion_cycle.time_left, BLUE, vec![]),
+        ("\n-----", CYAN, vec![]),
+    ], NewLine);
+    /*
+    println!("\n----------------------------------------");
     println!(" Plains of Eidolon (Cetus)");
     println!("   State: {}", cetus_state);
     println!("   Time until change: {}", world_state.cetus_cycle.time_left);
     println!("\n----------------------------------------");
-    let vallis_state = if world_state.vallis_cycle.is_warm { "Warm" } else { "Cold" };
     println!(" Orb Vallis");
     println!("   State: {}", vallis_state);
     println!("   Time until change: {}", world_state.vallis_cycle.time_left);
@@ -54,5 +83,6 @@ pub async fn worldstate() -> Result<(), Box<dyn Error>> {
     println!("   State: {}", world_state.cambion_cycle.active.to_uppercase());
     println!("   Time until change: {}", world_state.cambion_cycle.time_left);
     println!("\n----------------------------------------");
+*/
     Ok(())
 }
